73fe5d383bd49e2452f3232f91ee49f9d380844a
Marin Gilles <mrngille@gmail.com>
2015-03-16 10:59:43 +0100

    Original behaviour of axes.grid + working rc axes.grid.orientation

--- lib/matplotlib/axes/_base.py
+++ lib/matplotlib/axes/_base.py
@@ -905,6 +905,7 @@ class _AxesBase(martist.Artist):
         self.collections = []  # collection.Collection instances
         self.containers = []
 
+        self.grid(False) # Disable grid on init to use rcParameter
         self.grid(self._gridOn, which=rcParams['axes.grid.which'],
                     axis=rcParams['axes.grid.axis'])
         props = font_manager.FontProperties(
@@ -2185,10 +2186,6 @@ class _AxesBase(martist.Artist):
         if len(kwargs):
             b = True
         b = _string_to_bool(b)
-
-        # We need to disable both grids before replotting the wanted ones
-        self.xaxis.grid(False)
-        self.yaxis.grid(False)
 
         if axis == 'x' or axis == 'both':
             self.xaxis.grid(b, which=which, **kwargs)
